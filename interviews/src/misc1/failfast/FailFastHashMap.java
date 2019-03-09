9f7ca6d57654fcaac00b32f2cc5b5f07d3ffa044
cyrussimkhada <simkhadakrishna994@gmail.com>
2019-03-09 11:30:38 -0500

    added comment in fail fast

--- interviews/src/misc1/failfast/FailFastHashMap.java
+++ interviews/src/misc1/failfast/FailFastHashMap.java
@@ -15,6 +15,9 @@ public class FailFastHashMap {
 		Iterator<String> keys = map.keySet().iterator();
 		while(keys.hasNext()) {
 			System.out.println(map.get(keys.next()));
+/*
+ * If we added map object here, we are going to get CocnurrentModificationException. So HashMap is fail fast.
+ */
 			map.put("emp3", "sam");
 		}
 		
